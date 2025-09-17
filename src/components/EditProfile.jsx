d91c2b07af0bb088f201dfb5e21ee982a31d04f0
vrahul2698 <vrahulmba005@gmail.com>
2025-09-17 19:43:01 +0530

    Added Edit profile userCard  path correction

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -1,6 +1,6 @@
 
 import { useState } from "react";
-import UserCard from "./userCard";
+import UserCard from "./UserCard";
 import axios from "axios";
 import { BASE_URL } from "../utils/constants";
 import { useDispatch } from "react-redux";
